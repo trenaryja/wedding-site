38e985e4ed1a98684b967fb1f37f2b2deb42a26b
Justin Trenary <trenaryja@gmail.com>
2022-05-09 09:49:47 -0400

    first implementation of gallery component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,16 @@
 import { Grid } from '@chakra-ui/react'
+import Gallery, { links as galleryLinks } from '~/components/Gallery'
 import OurDate from '~/components/OurDate'
 import OurNames from '~/components/OurNames'
 
+export const links = () => [...galleryLinks()]
+
 export default function Index() {
 	return (
-		<Grid py={10} my={40} gap={40}>
+		<Grid py={10} my={20} gap={20} justifyContent='center'>
 			<OurNames />
 			<OurDate maxW='xs' />
+			<Gallery />
 		</Grid>
 	)
 }
