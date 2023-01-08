bf09c8871b5b0976f02dcfba34b81a96911899b6
Justin Trenary <trenaryja@gmail.com>
2023-01-08 09:13:00 -0500

    add people and update location

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
 
 			<iframe
 				title='map'
-				src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3036.400688727804!2d-79.9921213!3d40.4442677!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8834f3e203155389%3A0xab3d797cbc95e2d9!2sThe%20Grand%20Hall%20at%20The%20Pennsylvanian!5e0!3m2!1sen!2sus!4v1656912875889!5m2!1sen!2sus'
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.4811941454795!2d-80.06114388455859!3d40.46461837935958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8834f5cbfef83f51%3A0xf0f16a3405aab864!2sRoxian%20Theatre!5e0!3m2!1sen!2sus!4v1673167366206!5m2!1sen!2sus'
 				width='100%'
 				height={640 - 32}
 			/>
--- src/pages/people/index.tsx
+++ src/pages/people/index.tsx
@@ -1,5 +1,63 @@
-import { Heading } from '@chakra-ui/react'
+import { Box, BoxProps, Grid, Heading, Image, useBreakpointValue } from '@chakra-ui/react'
+
+const flatHexApectRatio = 2 / Math.sqrt(3)
+const flatHexClipPath = `polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);`
+const Hexagon = (props: BoxProps) => <Box clipPath={flatHexClipPath} {...props}></Box>
+
+type PersonProps = {
+	first: string
+	last: string
+	title: string
+}
+
+const Person = ({ first, last, title }: PersonProps) => (
+	<Box>
+		<Grid>
+			<Hexagon gridArea='x' bg='whiteAlpha.300' />
+			<Hexagon gridArea='x' bg='whiteAlpha.400' transform='scale(.9)' />
+			<Hexagon gridArea='x' bg='whiteAlpha.500' transform='scale(.8)' />
+			<Hexagon gridArea='x' bg='whiteAlpha.600' transform='scale(.7)' />
+			<Hexagon gridArea='x' bg='whiteAlpha.700' transform='scale(.6)' />
+			<Hexagon gridArea='x' bg='whiteAlpha.800' transform='scale(.5)' />
+			<Image
+				gridArea='x'
+				zIndex={1}
+				alt={first}
+				objectFit='cover'
+				clipPath='polygon(0% 0%, 100% 0%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);'
+				filter='grayscale(.75) contrast(1.25)'
+				sx={{ aspectRatio: `${flatHexApectRatio}` }}
+				src={`/people/${first}.png`}
+			/>
+		</Grid>
+		<Heading textAlign='center'>
+			{first} {last}
+		</Heading>
+		<Heading textAlign='center' size='md'>
+			{title}
+		</Heading>
+	</Box>
+)
+
+const people: PersonProps[] = [
+	{ first: 'Rachel', last: 'Hamilton', title: 'Bride' },
+	{ first: 'Justin', last: 'Trenary', title: 'Groom' },
+	{ first: 'Janice', last: 'Turton', title: 'Officiant ' },
+	{ first: 'Emily', last: 'Western', title: 'Maid of Honor' },
+	{ first: 'Amanda', last: 'Glines', title: 'Bridesmaid' },
+	{ first: 'Chase', last: 'Canterberry', title: 'Bridesman' },
+	{ first: 'Ross', last: 'Combs', title: 'Best Man' },
+	{ first: 'Josh', last: 'Trenary', title: 'Groomsman' },
+	{ first: 'Jay', last: 'Seghi', title: 'Groomsman' },
+]
 
 export default function Index() {
-	return <Heading>PEOPLE</Heading>
+	const columnCount = useBreakpointValue({ base: 1, sm: 2, md: 3 })
+	return (
+		<Grid gap={10} pt={10} gridTemplateColumns={`repeat(${columnCount}, 1fr)`}>
+			{people.map((person) => (
+				<Person key={person.first} {...person} />
+			))}
+		</Grid>
+	)
 }
