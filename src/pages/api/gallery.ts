f424070bbed50c854546a775fb174cba01253ced
Justin Trenary <trenaryja@gmail.com>
2023-06-27 00:18:35 -0400

    update gallery api

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -1,20 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { withSessionRoute } from '../../utils'
 
-const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { albumId } = req.query
-
 	const raw = await fetch(`https://photos.app.goo.gl/${albumId}`)
 	const source = await raw.text()
-
-	const links = new Set<string>()
-	let match: RegExpExecArray
-
-	while ((match = regex.exec(source))) {
-		links.add(match[1])
-	}
+	const regex = /https:\/\/lh3\.googleusercontent\.com\/pw\/.*?=w/g
+	const links = Array.from(source.matchAll(regex), (match) => `${match[0]}9999`)
 	res.json(Array.from(links))
 }
 
