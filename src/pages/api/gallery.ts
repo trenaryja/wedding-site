9f87fcbebd27533937e3a792a15126d02a32574b
Justin Trenary <trenaryja@gmail.com>
2023-07-06 17:12:43 -0400

    fix vercel puppeteer issue

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -1,10 +1,10 @@
+import chromium from 'chrome-aws-lambda'
 import { NextApiRequest, NextApiResponse } from 'next'
-import puppeteer from 'puppeteer'
 import { withSessionRoute } from '../../utils'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { albumId } = req.query
-	const browser = await puppeteer.launch({ headless: 'new' })
+	const browser = await chromium.puppeteer.launch()
 	const page = await browser.newPage()
 	await page.setViewport({ width: 1920, height: 99999 })
 	await page.goto(`https://photos.app.goo.gl/${albumId}`, { waitUntil: 'networkidle2' })
