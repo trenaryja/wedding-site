f7d023f3ee4ab196cb2ee9638b903ad39b607e78
Justin Trenary <trenaryja@gmail.com>
2023-07-06 20:40:01 -0400

    fix variable name and lockfile

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -5,8 +5,8 @@ import { withSessionRoute } from '../../utils'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { albumId } = req.query
   const url = `https://photos.app.goo.gl/${albumId}`
-  const test = await fetchImageUrls(url)
-  const imageUrls = test.map((x) => x.url)
+  const data = await fetchImageUrls(url)
+  const imageUrls = data.map((x) => x.url)
   res.json(imageUrls)
 }
 
