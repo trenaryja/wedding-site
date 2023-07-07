ad5afb60d19e3a8e31c5ad08610156ca9752c2c2
Justin Trenary <trenaryja@gmail.com>
2023-07-06 20:38:48 -0400

    ditch puppeteer in favor of google-photos-album-image-url-fetch

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -1,26 +1,12 @@
+import { fetchImageUrls } from 'google-photos-album-image-url-fetch'
 import { NextApiRequest, NextApiResponse } from 'next'
-import puppeteer from 'puppeteer-core'
 import { withSessionRoute } from '../../utils'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { albumId } = req.query
   const url = `https://photos.app.goo.gl/${albumId}`
-
-  const browser = await puppeteer.connect({
-    defaultViewport: { width: 1920, height: 99999 },
-    browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BLESS_TOKEN}`,
-  })
-
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1920, height: 99999 })
-  await page.goto(url, { waitUntil: 'networkidle2' })
-
-  const imageUrls = await page.evaluate(() => {
-    const regex = /https:\/\/lh3\.googleusercontent\.com\/pw\/.*?=w/g
-    const links = Array.from(document.body.innerHTML.matchAll(regex), (match) => `${match[0]}500`)
-    return links.filter((link, index, self) => self.indexOf(link) === index)
-  })
-
+  const test = await fetchImageUrls(url)
+  const imageUrls = test.map((x) => x.url)
   res.json(imageUrls)
 }
 
