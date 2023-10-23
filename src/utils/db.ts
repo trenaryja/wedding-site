6013a49840737b75985617f7fb0b48d78f732fb2
Justin Trenary <trenaryja@gmail.com>
2023-10-23 01:04:43 -0400

    remove prisma

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,13 +1,5 @@
-import { User } from '@prisma/client'
-import { deleteJson, getJson, postJson, putJson } from './api'
+import { getJson, putJson } from './api'
 import { NotionUser } from './notion'
-
-export const getUsers = async () => await getJson<User[]>('/api/db/users')
-
-export const getUser = async (id: number) => await getJson<User>('/api/db/user', { id })
-export const createUser = async (data: User) => await postJson<User>('/api/db/user', { data })
-export const updateUser = async (id: number, data: User) => await putJson<User>('/api/db/user', { id }, { data })
-export const deleteUser = async (id: number) => await deleteJson<User>('/api/db/user', { id })
 
 export const getNotionUsers = async () => await getJson<NotionUser[]>('/api/notion')
 export const updateNotionUser = async (id: string, data: NotionUser) =>
